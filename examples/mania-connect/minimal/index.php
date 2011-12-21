e5ff1cc4c52e284579024a71db679f3a4a96f12a
gou1 <maximeraoust@gmail.com>
2011-12-21 15:45:35 +0000

    Even simplier
    git-svn-id: http://maniaplanet-ws-sdk.googlecode.com/svn/trunk@88 27f79ad1-2b5e-7b61-3877-dbde595bd42e

--- examples/mania-connect/minimal/index.php
+++ examples/mania-connect/minimal/index.php
@@ -1,38 +1,25 @@
 <?php
-/**
- * Maniaplanet Web Services SDK for PHP
- * Minimal ManiaConnect example
- *
- * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
- * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
- * @author      $Author$:
- * @version     $Revision$:
- * @date        $Date$:
- */
 require_once __DIR__.'/../../../libraries/autoload.php';
-
-define('API_USERNAME', 'api_username');
-define('API_PASSWORD', 'api_password');
 
 try
 {
-	$maniaconnect = new \Maniaplanet\WebServices\ManiaConnect\Player(API_USERNAME, API_PASSWORD);
-	$loginURL = $maniaconnect->getLoginURL();
+	$maniaconnect = new \Maniaplanet\WebServices\ManiaConnect\Player('your_api_username', 'your_api_password');
+	$loginURL = $maniaconnect->getLoginURL('basic');
 	$player = $maniaconnect->getPlayer();
 }
-catch(\Maniaplanet\WebServices\Exception $e) 
+catch(\Maniaplanet\WebServices\Exception $e)
 {
-	// Errors are ignored
+	// We ignore errors
 }
 
 if($player)
 {
 	// Player is logged in, do whatever you want
+	echo $player->login;
 }
 else
 {
 	// Player is not logged in, link or redirect to $loginURL
+	printf('<a href="%s">Login</a>', htmlentities($loginURL));
 }
-
-
 ?>
