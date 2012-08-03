3292edfe3b2176f0ad4f90564fd7986e6d9add08
magnetik <baptiste33@gmail.com>
2012-08-03 10:26:35 +0000

    Adding ManiaFlash to SDK
    git-svn-id: http://maniaplanet-ws-sdk.googlecode.com/svn/trunk@142 27f79ad1-2b5e-7b61-3877-dbde595bd42e

---
+++ libraries/Maniaplanet/WebServices/ManiaFlash.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Maniaplanet Web Services SDK for PHP
+ *
+ * @see		    http://code.google.com/p/maniaplanet-ws-sdk/
+ * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @author      $Author$:
+ * @version     $Revision$:
+ * @date        $Date$:
+ */
+
+namespace Maniaplanet\WebServices;
+
+class ManiaFlash extends HTTPClient
+{
+	/**
+	 * Return informations about a ManiaFlash
+	 * @param string $id
+	 * @return Object
+	 *			- name
+	 *			- description
+	 *			- id
+	 * @throws Exception
+	 */
+	public function getChannel($id)
+	{
+		if(!$id)
+		{
+			throw new Exception('Invalid login');
+		}
+		return $this->execute('GET', '/maniaflash/channels/%s/', array($id));
+	}
+
+	/**
+	 * Return latest messages of a channel
+	 * @param string $id
+	 * @return Object[]
+	 *				- id
+	 *				- author
+	 *				- dateCreated
+	 *				- message
+	 *				- link
+	 * @throws Exception
+	 */
+	public function getMessages($id, $offset = 0, $length = 10)
+	{
+		if(!$id)
+		{
+			throw new Exception('Invalid login');
+		}
+		return $this->execute('GET', '/maniaflash/channels/%s/messages/?offset=%d&length=%d', array($id, $offset, $length));
+	}
+
+	/**
+	 * Publish a message on a maniaflash channel
+	 * @param string $channelId
+	 * @param string $message
+	 * @param string $link
+	 * @param string $iconStyle
+	 * @param string $iconSubStyle
+	 * @return type
+	 */
+	public function postMessage($channelId, $message, $link = null, $iconStyle = null, $iconSubStyle = null)
+	{
+		return $this->execute('POST', '/maniaflash/channels/%s/', array(
+			$channelId,
+			array(
+				'message' => $message,
+				'link' => $link,
+				'iconStyle' => $iconStyle,
+				'iconSubStyle' => $iconSubStyle)));
+	}
+}
