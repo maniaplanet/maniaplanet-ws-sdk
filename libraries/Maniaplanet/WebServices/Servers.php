2a30aba9eb6fac8c4c6fecbd022e0b2114af2605
Philippe Melot <melot.philippe@gmail.com>
2012-08-29 10:06:16 +0000

    add method to get the server list with filters
    git-svn-id: http://maniaplanet-ws-sdk.googlecode.com/svn/trunk@157 27f79ad1-2b5e-7b61-3877-dbde595bd42e

--- libraries/Maniaplanet/WebServices/Servers.php
+++ libraries/Maniaplanet/WebServices/Servers.php
@@ -59,14 +59,41 @@ class Servers extends HTTPClient
 		}
 		return $this->execute('GET', '/servers/%s/favorited/', array($login));
 	}
-	
+
+	/**
+	 * Return the report abuses created on the given login
+	 * @param string $login
+	 * @return object[]
+	 * @throws Exception
+	 */
 	function getReportAbuses($login)
 	{
 		if(!$login)
 		{
 			throw new Exception('Invalid login');
 		}
-		return $this->execute('GET', '/report-abuse/list/%s/',array($login));
+		return $this->execute('GET', '/report-abuse/list/%s/', array($login));
+	}
+
+	/**
+	 * @param array[] $filters This should be an associative array, it can contain any of the following values with the 
+	 * correct key:
+	 * environment Canyon or Storm
+	 * title the Title idString
+	 * playersMin	int		Minimum number of player connected on the server
+	 * playersMax	int		Maximum number of player connected on the server
+	 * hideFull		bool	If true, full servers will not be displayed
+	 * visibility	string	It can take one of those 3 values: all, public or private
+	 * zone			string	The path to a zone, for example: World|France. It will display every server in France subzones
+	 * mode			mixed	it can be one of the GameMode class constant, or directly the game mode name itself (TimeAttack, Melee, ...)
+	 * offset		int		The offset in the list where the results will start
+	 * length		int		Number of elements returned
+	 * @return object[]
+	 */
+	function getFilteredList(array $filters = array())
+	{
+		$params = http_build_query($filters, '', '&');
+		return $this->execute('GET', '/servers/?'.$params);
 	}
 
 }
