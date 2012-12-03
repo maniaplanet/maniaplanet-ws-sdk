88eb18625203c494399782a1b83005d57fbbdc4c
magnetik <baptiste33@gmail.com>
2012-12-03 10:15:02 +0000

    Adding ladder limit min/max server filtering
    git-svn-id: http://maniaplanet-ws-sdk.googlecode.com/svn/trunk@171 27f79ad1-2b5e-7b61-3877-dbde595bd42e

--- libraries/Maniaplanet/WebServices/Servers.php
+++ libraries/Maniaplanet/WebServices/Servers.php
@@ -80,18 +80,20 @@ class Servers extends HTTPClient
 	}
 
 	/**
-	 * @param array[] $filters This should be an associative array, it can contain any of the following values with the 
+	 * @param array[] $filters This should be an associative array, it can contain any of the following values with the
 	 * correct key:
 	 * environment string Canyon or Storm
 	 * title string the Title idString
-	 * playersMin	int		Minimum number of player connected on the server
-	 * playersMax	int		Maximum number of player connected on the server
-	 * hideFull		bool	If true, full servers will not be displayed
-	 * visibility	string	It can take one of those 3 values: all, public or private
-	 * zone			string	The path to a zone, for example: World|France. It will display every server in France subzones
-	 * mode			mixed	it can be one of class constant, or directly the game mode name itself (TimeAttack, Melee, ...)
-	 * offset		int		The offset in the list where the results will start
-	 * length		int		Number of elements returned
+	 * playersMin		int		Minimum number of player connected on the server
+	 * playersMax		int		Maximum number of player connected on the server
+	 * hideFull			bool	If true, full servers will not be displayed
+	 * visibility		string	It can take one of those 3 values: all, public or private
+	 * zone				string	The path to a zone, for example: World|France. It will display every server in France subzones
+	 * mode				mixed	it can be one of class constant, or directly the game mode name itself (TimeAttack, Melee, ...)
+	 * ladderLimitMin	int		Ladder server limit min (ie: 70000)
+	 * ladderLimitMax	int		Ladder server limit max (ie: 100000)
+	 * offset			int		The offset in the list where the results will start
+	 * length			int		Number of elements returned
 	 * @return object[]
 	 */
 	function getFilteredList(array $filters = array())
