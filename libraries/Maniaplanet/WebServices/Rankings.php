e9bfe750f4b22e1e0e6de3269b0765dc63927d4c
magnetik <baptiste33@gmail.com>
2013-05-15 14:37:56 +0000

    Changed Rankings to use only one class
    git-svn-id: http://maniaplanet-ws-sdk.googlecode.com/svn/trunk@193 27f79ad1-2b5e-7b61-3877-dbde595bd42e

---
+++ libraries/Maniaplanet/WebServices/Rankings.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * @version     $Revision$:
+ * @author      $Author$:
+ * @date        $Date$:
+ */
+namespace Maniaplanet\WebServices;
+
+class Rankings extends HTTPClient
+{
+	/**
+	 * Get player ranking for the give title
+	 * @param string $titleId
+	 * @param string $login
+	 * @return Object
+	 * @throws Exception
+	 */
+	function getMultiplayerPlayer($titleId, $login)
+	{
+		if(!$login)
+		{
+			throw new Exception('Invalid login');
+		}
+		return $this->execute('GET', $this->getPrefixEndpoint($titleId).'/rankings/multiplayer/player/%s/?title=%s', array($login, $titleId));
+	}
+
+	/**
+	 *
+	 * @param string $titleId
+	 * @param int $offset
+	 * @param int $length
+	 * @return Object
+	 */
+	function getMultiplayerWorld($titleId, $offset = 0, $length = 100)
+	{
+		return $this->execute('GET', $this->getPrefixEndpoint($titleId).'/rankings/multiplayer/zone/?offset=%d&length=%d&title=%s', array($offset, $length, $titleId));
+	}
+
+	/**
+	 * @param string $titleId
+	 * @param string $path
+	 * @param int $offset
+	 * @param int $length
+	 * @return Object
+	 * @throws Exception
+	 */
+	function getMultiplayerZone($titleId, $path, $offset = 0, $length = 100)
+	{
+		if(!$path)
+		{
+			throw new Exception('Invalid zone path');
+		}
+		return $this->execute('GET', $this->getPrefixEndpoint($titleId).'/rankings/multiplayer/zone/%s/?offset=%d&length=%d&title=%s',
+						array($path, $offset, $length, $titleId));
+	}
+
+	/**
+	 * @param string $titleId
+	 * @param string $login
+	 * @return Object
+	 * @throws Exception
+	 */
+	function getSoloPlayer($titleId, $login)
+	{
+		if(!$login)
+		{
+			throw new Exception('Invalid login');
+		}
+		return $this->execute('GET', $this->getPrefixEndpoint($titleId).'/rankings/solo/player/%s/?title=%s', array($login, $titleId));
+	}
+
+	/**
+	 * @param string $titleId
+	 * @param int $offset
+	 * @param int $length
+	 * @return Object
+	 */
+	function getSoloWorld($titleId, $offset = 0, $length = 100)
+	{
+		return $this->execute('GET', $this->getPrefixEndpoint($titleId).'/rankings/solo/zone/?offset=%d&length=%d&title=%s', array($offset, $length, $titleId));
+	}
+
+	/**
+	 * @param string $titleId
+	 * @param string $path
+	 * @param int $offset
+	 * @param int $length
+	 * @return Object
+	 * @throws Exception
+	 */
+	function getSoloZone($titleId, $path, $offset = 0, $length = 100)
+	{
+		if(!$path)
+		{
+			throw new Exception('Invalid zone path');
+		}
+		return $this->execute('GET', $this->getPrefixEndpoint($titleId).'/rankings/solo/zone/%s/?offset=%d&length=%d&title=%s', array($path, $offset, $length, $titleId));
+	}
+
+	/**
+	 * @param string $titleId
+	 * @param string $challengeuid
+	 * @param int $offset
+	 * @param int $length
+	 * @return type
+	 * @throws Exception
+	 */
+	function getSoloChallengeWorld($titleId, $challengeuid, $offset = 0, $length = 100)
+	{
+		if(!$challengeuid)
+		{
+			throw new Exception('Invalid challenge UID');
+		}
+		return $this->execute('GET', $this->getPrefixEndpoint($titleId).'/rankings/solo/challenge/%s/?offset=%d&length=%d&title=%s',
+						array($challengeuid, $offset, $length, $titleId));
+	}
+
+	/**
+	 * @param string $titleId
+	 * @param string $challengeuid
+	 * @param string $path
+	 * @param int $offset
+	 * @param int $length
+	 * @return type
+	 * @throws Exception
+	 */
+	function getSoloChallengeZone($titleId, $challengeuid, $path, $offset = 0, $length = 100)
+	{
+		if(!$challengeuid)
+		{
+			throw new Exception('Invalid challenge UID');
+		}
+		return $this->execute('GET', $this->getPrefixEndpoint($titleId).'/rankings/solo/challenge/%s/%s/?offset=%d&length=%d&title=%s',
+						array($challengeuid, $path, $offset, $length, $titleId));
+	}
+
+	protected function getPrefixEndpoint($titleId)
+	{
+		switch($titleId)
+		{
+			case 'SMStorm':
+				return '/storm';
+
+			case 'TMCanyon':
+				return '/canyon';
+
+			case 'SMStormRoyal@nadeolabs':
+				return '/royal';
+
+			case 'TMStadium':
+				return '/stadium';
+
+			default:
+				return '/title';
+		}
+	}
+}
+?>
