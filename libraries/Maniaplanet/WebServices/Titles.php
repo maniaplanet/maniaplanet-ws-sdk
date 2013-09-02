0211aacd092bb276d23373c742a4f6a6f56fae96
magnetik <baptiste33@gmail.com>
2013-09-02 13:09:04 +0000

    Adding titles endpoint
    git-svn-id: http://maniaplanet-ws-sdk.googlecode.com/svn/trunk@206 27f79ad1-2b5e-7b61-3877-dbde595bd42e

---
+++ libraries/Maniaplanet/WebServices/Titles.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Maniaplanet Web Services SDK for PHP
+ *
+ * @see		    http://code.google.com/p/maniaplanet-ws-sdk/
+ * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @author      $Author$:
+ * @version     $Revision$:
+ * @date        $Date$:
+ */
+
+namespace Maniaplanet\WebServices;
+
+class Titles extends HTTPClient
+{
+	function get($idString)
+	{
+		return $this->execute('GET', '/titles/%s/', array($idString));
+	}
+}
